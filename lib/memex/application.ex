4f7fe210eea0c631e5af9b8459f1f040f9a5ca54
JediLuke <batcoder@letterboxes.org>
2021-06-22 13:44:35 -0500

    Aesthetic improvements to application.ex

--- lib/memex/application.ex
+++ lib/memex/application.ex
@@ -1,20 +1,19 @@
 defmodule Memex.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
-
   use Application
 
+
   def start(_type, _args) do
-    # List all child processes to be supervised
+
     children = [
-      # Starts a worker by calling: Memex.Worker.start_link(arg)
-      # {Memex.Worker, arg},
+      Memex.BootCheck,
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Memex.Supervisor]
+    opts = [
+          name: Memex.Supervisor,
+      strategy: :one_for_one
+    ]
+
     Supervisor.start_link(children, opts)
   end
 end
