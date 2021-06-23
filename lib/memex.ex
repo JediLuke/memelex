22c39c7afa61eb0879615bce7cf40cf99f76b61c
JediLuke <batcoder@letterboxes.org>
2021-06-23 16:51:46 -0500

    just remove some boilerplate

--- lib/memex.ex
+++ lib/memex.ex
@@ -3,16 +3,4 @@ defmodule Memex do
   Documentation for Memex.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Memex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
