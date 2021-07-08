44f3be74378e76c21df5e2b5cbf5a9c8dc4d604e
JediLuke <batcoder@letterboxes.org>
2021-07-08 17:48:31 -0500

    Able to add new blog ideas with tags now

--- lib/api/my_blog.ex
+++ lib/api/my_blog.ex
@@ -5,6 +5,13 @@ defmodule Memex.My.Blog do
     new_idea(%{title: title})
   end
 
+  def new_idea(%{tags: tlist} = params) when is_list(tlist) do
+    validate_tag_list!(tlist)
+    params
+    |> Map.merge(%{tags: tlist ++ ["my_blog", "idea"]})
+    |> Memex.My.Wiki.new_tidbit()
+  end
+
   def new_idea(params) when is_map(params) do
     params
     |> Map.merge(%{tags: ["my_blog", "idea"]})
@@ -23,6 +30,18 @@ defmodule Memex.My.Blog do
             and
           tidbit.tags |> Enum.member?("idea")
         end)
+  end
+
+
+  def validate_tag_list!([]) do
+    true
+  end
+  def validate_tag_list!([tag|rest]) when is_bitstring(tag) do
+    validate_tag_list!(rest)
+  end
+  def validate_tag_list!([tag|_rest]) do # matches anything besides a string 
+    context = %{invalid_tag: tag}
+    raise "an invalid tag was passed in via the tag list. #{inspect context}"
   end
 
 end
