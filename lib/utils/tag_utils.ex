9b70ad18a28e6aaec72f0d3a1f4a491de8deb002
JediLuke <batcoder@letterboxes.org>
2021-07-10 19:08:40 -0500

    Rename the module for tag_utils

---
+++ lib/utils/tag_utils.ex
@@ -0,0 +1,17 @@
+defmodule Memex.Utils.Tags do
+
+
+  def validate_tag_list!([]) do
+    true
+  end
+
+  def validate_tag_list!([tag|rest]) when is_bitstring(tag) do
+    validate_tag_list!(rest)
+  end
+
+  def validate_tag_list!([tag|_rest]) do # matches anything besides a string 
+    context = %{invalid_tag: tag}
+    raise "an invalid tag was passed in via the tag list. #{inspect context}"
+  end
+
+end
