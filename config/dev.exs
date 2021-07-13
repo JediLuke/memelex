255ce0b276b0d23c05ea80c1c4d94a9069778116
JediLuke <batcoder@letterboxes.org>
2021-07-13 13:14:04 -0500

    Simplest form of backups you can get

--- config/dev.exs
+++ config/dev.exs
@@ -4,5 +4,6 @@ import Config
 config :memex,
   environment: %{
     name: "JediLuke",
-    memex_directory: "/home/pi/memex/JediLuke"
+    memex_directory: "/home/pi/memex/JediLuke",
+    backups_directory: "/media/pi/Samsung USB/memex_backups"
   }
