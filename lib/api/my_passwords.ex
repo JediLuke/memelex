9e49d05a645c8b9033f1e7ce27fe933cdd828439
JediLuke <batcoder@letterboxes.org>
2021-07-23 17:36:16 -0500

    moev find into My.Passwords

--- lib/api/my_passwords.ex
+++ lib/api/my_passwords.ex
@@ -1,6 +1,8 @@
 defmodule Memex.My.Passwords do
   alias Memex.Env.PasswordManager
 
+  @simularity_cutoff 0.72 # how close we need labels to be in our search algorithm
+
   def new(params) do # API sugar
     create(params)
   end
@@ -14,10 +16,27 @@ defmodule Memex.My.Passwords do
     GenServer.call(PasswordManager, {:new_password, new_password})
   end
 
-  def find(label) do
-    {:ok, password} =
-       GenServer.call(PasswordManager, {:find_unredacted_password, label})
-    password
+
+  def find(%Memex.Password{} = password) do
+    case GenServer.call(PasswordManager, {:find_unredacted_password, password}) do
+      {:ok, %Memex.Password{} = unredacted_password} ->
+          unredacted_password
+      {:error, "password not found"} ->
+          :not_found
+    end
+  end
+
+  # search through the labels
+  def find(search_term) when is_binary(search_term) do
+    password =
+      list |> Enum.find(:not_found,
+                        & String.jaro_distance(&1.label, search_term) > @simularity_cutoff)
+
+    if password == :not_found do
+      :not_found
+    else
+      find(password)
+    end
   end
 
   def list do
