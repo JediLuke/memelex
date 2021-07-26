09b88b9b5a4396dba943060c3afafe7e7253d009
JediLuke <batcoder@letterboxes.org>
2021-07-25 21:03:28 -0500

    This probably was a step forward, even though I have come to hate "stringify datetimes"

--- lib/api/my_journal.ex
+++ lib/api/my_journal.ex
@@ -1,5 +1,7 @@
 defmodule Memex.My.Journal do
   alias Memex.Env.WikiManager
+  alias Memex.Facts.GregorianCalendar
+  alias Memex.Utils.StringifyDateTimes
   require Logger
 
   @doc ~s(Opens today's Journal entry, with a fresh timestamp appended.)
@@ -78,41 +80,31 @@ defmodule Memex.My.Journal do
 
   @doc ~s(Contruct a title string for my Journal for a given datetime.)
   def journal_page_title(datetime) do
-    "Journal of #{Memex.My.nickname()} ~ #{day_and_month(datetime)}, #{datetime.year |> Integer.to_string()}"
+    day_and_month =
+      datetime |> StringifyDateTimes.format("day_xx_of_month")
+    year =
+      datetime |> StringifyDateTimes.format("year_as_XXXX")
+
+    "Journal of #{Memex.My.nickname()} ~ #{day_and_month}, #{year}"
   end
 
-  @doc ~s(Return a string containing the day of the month in long-form.)
-  def day_and_month(datetime) do
-    day_of_the_week =
-      datetime |> DateTime.to_date() |> Date.day_of_week() |> day_name()
-    month_name = 
-      datetime.month |> month_name() # e.g. July or August
-    day_of_the_month =
-      datetime.day |> pad_to_double_digit_string() # e.g. "01", "02" or "29"
-
-    # e.g. 'Wednesday 29 of June'
-    "#{day_of_the_week} #{day_of_the_month} of #{month_name}"
-  end
 
   @doc ~s(Return the filepath for the Journal entry for a specific DateTime. If it doesn't exist yet, then create it.)
   def journal_entry_filepath(datetime) do
 
     # we save Journal files in a structure, `memex/environment/journal/year/month/xx-day.txt`
     journal_directory =
-      memex_directory()
+      Memex.Utils.ToolBag.memex_directory()
       |> Path.join("/journal")
-      |> Path.join("/#{datetime.year  |> Integer.to_string()}")
-      |> Path.join("/#{datetime.month |> month_name()}")
+      |> Path.join("/#{datetime.year |> Integer.to_string()}")
+      |> Path.join("/#{datetime.month |> GregorianCalendar.month_name()}")
 
     if not File.exists?(journal_directory) do
       File.mkdir_p(journal_directory)
     end
 
-    {day_of_the_week_abbreviation, _rest} =
-      datetime |> DateTime.to_date() |> Date.day_of_week() |> day_name() |> String.split_at(3)
-    day_of_the_month =
-      datetime.day |> pad_to_double_digit_string() # e.g. "01", "02" or "29"
-    filename = "#{day_of_the_month}-#{day_of_the_week_abbreviation}.txt"
+    filename =
+      StringifyDateTimes.format(datetime, :journal_format) <> ".txt" # e.g. "23-Fri.txt"
 
     journal_entry_file =
       journal_directory |> Path.join("/#{filename}")
@@ -145,40 +137,4 @@ defmodule Memex.My.Journal do
     :ok
   end
 
-  def memex_directory do
-    {:ok, dir} = WikiManager |> GenServer.call(:whats_the_current_memex_directory?)
-    dir
-  end
-
-  # turn an integer 'day of the month' into a string which is always double-digit
-  def pad_to_double_digit_string(x) when x in [1,2,3,4,5,6,7,8,9] do
-    "0" <> Integer.to_string(x)
-  end
-  def pad_to_double_digit_string(x) when is_integer(x) and x > 9 and x <= 31 do # max 31 days in a month or week
-    Integer.to_string(x)
-  end
-
-  # used to compute the title for today's Journal entry
-  def day_name(1), do: "Monday"
-  def day_name(2), do: "Tuesday"
-  def day_name(3), do: "Wednesday"
-  def day_name(4), do: "Thursday"
-  def day_name(5), do: "Friday"
-  def day_name(6), do: "Saturday"
-  def day_name(7), do: "Sunday"
-
-  # used to compute the title for today's Journal entry
-  def month_name(1), do: "January"
-  def month_name(2), do: "February"
-  def month_name(3), do: "March"
-  def month_name(4), do: "April"
-  def month_name(5), do: "May"
-  def month_name(6), do: "June"
-  def month_name(7), do: "July"
-  def month_name(8), do: "August"
-  def month_name(9), do: "September"
-  def month_name(10), do: "October"
-  def month_name(11), do: "November"
-  def month_name(12), do: "December"
- 
 end
---
+++ lib/utils/stringify_datetimes.ex
@@ -0,0 +1,69 @@
+defmodule Memex.Utils.StringifyDateTimes do
+
+
+  @doc """
+  This function takes in a %DateTime{} and converts it to a consistent
+  string format. This is needed for things like:
+  - creating the name of today's Journal file
+  - creating the filename for Memex backups
+  - etc...
+
+  e.g. 07-Thu
+  """
+  def format(datetime, :journal_format) do
+
+    day_of_the_month =
+      datetime.day |> pad_to_double_digit_string() # e.g. "01", "02" or "29"
+
+    {day_of_the_week_abbreviation, _rest} =
+      datetime
+      |> DateTime.to_date()
+      |> Date.day_of_week()
+      |> Memex.Facts.GregorianCalendar.day_name()
+      |> String.split_at(3)
+
+    "#{day_of_the_month}-#{day_of_the_week_abbreviation}"
+  end
+
+  @doc ~s(Return a string containing the day of the month in long-form.)
+  def format(datetime, "day_xx_of_month") do
+    day_of_the_week  = datetime |> format(extract: "day_of_the_week")
+    month_name       = datetime |> format(extract: "month_name")
+    day_of_the_month = datetime |> format(extract: "day_of_the_month")
+
+    # e.g. 'Wednesday 29 of June'
+    "#{day_of_the_week} #{day_of_the_month} of #{month_name}"
+  end
+
+  @doc ~s(Return a string containing the day of the month in long-form.)
+  def format(%{year: year}, "year_as_XXXX") when is_integer(year) do
+    year |> Integer.to_string()
+  end
+
+  def format(datetime, extract: "day_of_the_week") do
+    datetime
+    |> DateTime.to_date()
+    |> Date.day_of_week()
+    |> Memex.Facts.GregorianCalendar.day_name()
+  end
+
+  def format(datetime, extract: "month_name") do
+    datetime.month |> Memex.Facts.GregorianCalendar.month_name() # e.g. July or August
+  end
+
+  def format(datetime, extract: "day_of_the_month") do
+    datetime.day |> pad_to_double_digit_string() # e.g. "01", "02" or "29"
+  end
+
+  # turn an integer 'day of the month' into a string which is always double-digit
+  def pad_to_double_digit_string(x) when x in [1,2,3,4,5,6,7,8,9] do
+    "0" <> Integer.to_string(x)
+  end
+  def pad_to_double_digit_string(x) when x in ["1","2","3","4","5","6","7","8","9"] do
+    "0" <> x
+  end
+  def pad_to_double_digit_string(x) when is_integer(x) and x > 9 and x <= 31 do # max 31 days in a month or week
+    Integer.to_string(x)
+  end 
+
+end
